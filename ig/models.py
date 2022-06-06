00289da177d239511e423bdacbca168ad883a788
SarahKamunya <sarahkamunya254@gmail.com>
2022-06-06 17:29:49 +0300

    add Image model with image,name,caption,likes,comments fields to capture post/image data

--- ig/models.py
+++ ig/models.py
@@ -1,5 +1,13 @@
+import profile
 from django.db import models
 from django.contrib.auth.models import User
+from django.forms import ImageField
 
 
 # Create your models here.
+class Image(models.Model):
+    name = models.CharField(max_length=100)
+    image = ImageField(upload_to ='photos/')
+    caption = models.CharField(max_length=500)
+    profile = models.ForeignKey(User,on_delete=models.CASCADE)
+    user = models.ForeignKey(Profile, on_delete=models.CASCADE, default=None)
--- instagram/settings.py
+++ instagram/settings.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'ig',
+    'bootstrap4',
+    'registration',
 ]
 
 MIDDLEWARE = [
