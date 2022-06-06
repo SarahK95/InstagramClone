323afdc91de89ed66d114079c8cf2222c41d6ced
SarahKamunya <sarahkamunya254@gmail.com>
2022-06-06 23:06:15 +0300

    add profile.html page for users to enter their profile, update url path for profile

---
+++ ig/urls.py
@@ -0,0 +1,13 @@
+from django.conf.urls import url
+from . import views
+from django.conf import settings
+from django.conf.urls.static import static
+from django.urls import path
+
+urlpatterns = [
+    path('',views.first,name='fistpage'),
+    path('home/',views.home,name='ighome'),
+    path('update-profile/',views.updateProfile,name='editProf'),
+    path('upload/',views.uploadPic,name='igFeed'),
+]
+
--- ig/views.py
+++ ig/views.py
@@ -12,7 +12,7 @@ from django.http import HttpResponse,Http404,HttpResponseRedirect
 # first page user sees basically loginpage
 def first(request):
     form = forms.AuthenticationForm
-    return render(request, 'registration/login.html', locals())
+    return render(request, 'registration/login.html', {'form':form})
 
 # homepage 
 @login_required(login_url='/accounts/login')
@@ -24,7 +24,59 @@ def home(request):
     profile = Profile.objects.all()
     likes = Likes.objects.all()
     
-    return render (request, 'home.html', locals())
+    context ={
+        'likesForm':likesForm,
+        'commentForm':commentForm,
+        'images':images,
+        'user':user,
+        'profile':profile,
+        'likes':likes
+    }
+    
+    return render (request, 'home.html', context)
+
+def likes(request, image_id):
+    likesForm = LikesForm()
+    post = Likes.objects.create(user=request.user, image=get_object_or_404(Image, pk=image_id),likes=1)
+    post.save()
+    print(post)
+    return redirect('ighome')
+
+def comments(request, image_id):
+    commentsForm = CommentsForm()
+    if request.method =='POST':
+        commentsForm = CommentsForm(request.POST)
+        if commentsForm.is_valid():
+            form = commentsForm.save(commit=False)
+            form.user=request.user
+            form.image = get_object_or_404(Image, pk=image_id)
+            form.save()
+    return redirect ('ighome') 
+
+@login_required(login_url='/accounts/login')
+def profilePage(request):
+    likesForm = LikesForm
+    commentForm = CommentsForm
+    images = Image.objects.all()
+    user = request.user.get_username()
+    current_user = request.user
+    cphoto = Image.objects.filter(profile=current_user.id)
+    profile = Profile.objects.all()
+    likes = Likes.objects.all()
+    
+    context ={
+        'likesForm':likesForm,
+        'commentForm':commentForm,
+        'images':images,
+        'user':user,
+        'profile':profile,
+        'likes':likes,
+        'cphoto':cphoto
+    }
+    
+    return render (request, 'profile.html', context)
+           
+    
 
 
 
