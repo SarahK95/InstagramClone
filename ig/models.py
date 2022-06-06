febf48f347eac1063b046942218403b2d0ff1a7e
SarahKamunya <sarahkamunya254@gmail.com>
2022-06-06 17:33:02 +0300

    add save_image() method to save images to db

--- ig/models.py
+++ ig/models.py
@@ -11,3 +11,8 @@ class Image(models.Model):
     caption = models.CharField(max_length=500)
     profile = models.ForeignKey(User,on_delete=models.CASCADE)
     user = models.ForeignKey(Profile, on_delete=models.CASCADE, default=None)
+    likes = models.ManyToManyField(User,blank=True, default=None)
+    comment = models.ManyToManyField(User,default=None, blank=True,related_name='comments')
+    
+    def save_image(self):
+        self.save()
