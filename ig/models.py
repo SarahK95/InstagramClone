29995bd4e909f654ed9a1e88cf13323472dd061a
SarahKamunya <sarahkamunya254@gmail.com>
2022-06-06 17:52:09 +0300

    add Profile Model to add user bio and profile pic

--- ig/models.py
+++ ig/models.py
@@ -1,3 +1,4 @@
+from distutils.command.upload import upload
 import profile
 from django.db import models
 from django.contrib.auth.models import User
@@ -27,7 +28,16 @@ class Image(models.Model):
         ordering = ['name']    
         
 
-@classmethod
-def images(cls):
-    images = cls.objects.filter(id = Image.id)
-    return images        
+    @classmethod
+    def images(cls):
+        images = cls.objects.filter(id = Image.id)
+        return images     
+    
+    def __str__(self):
+        return str(self.name)
+    
+    
+class Profile(models.Model):  
+    prof_pic = ImageField(upload_to ='photos/', blank=True) 
+    bio = models.CharField(max_length=500)
+    user = models.OneToOneField('auth.User',on_delete=models.CASCADE, default=None)    
